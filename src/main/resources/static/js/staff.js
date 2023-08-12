ea19aecdb311bb3462f11f072189bbd83c8e62a0
Oleksandr Denysov <sosed0000@gmail.com>
2023-08-12 16:53:20 +0300

    Styles added

--- src/main/resources/static/js/staff.js
+++ src/main/resources/static/js/staff.js
@@ -20,32 +20,38 @@ stompClient.connect({}, () => {
 function addOrderToList(order) {
     if (order.status !== 'COMPLETED') {
         const orderList = document.getElementById('orderList');
-        const orderItem = document.createElement('li');
-        orderItem.dataset.orderId = order.id;
-        orderItem.textContent = `${order.description} `;
+        const row = orderList.insertRow();
+        row.dataset.orderId = order.id;
 
+        const orderIdCell = row.insertCell();
+        orderIdCell.textContent = order.id;
+
+        const orderDescriptionCell = row.insertCell();
+        orderDescriptionCell.textContent = order.description;
+
+        const statusCell = row.insertCell();
         const statusSpan = document.createElement('span');
-        statusSpan.textContent = `(${order.status})`;
+        statusSpan.textContent = order.status;
         statusSpan.className = 'status';
-        orderItem.appendChild(statusSpan);
+        statusCell.appendChild(statusSpan);
 
+        const actionCell = row.insertCell();
         const takeButton = document.createElement('button');
         takeButton.textContent = "Take Order";
         takeButton.addEventListener('click', () => takeOrder(order.id));
         takeButton.id = `takeButton_${order.id}`;
-        orderItem.appendChild(takeButton);
+        actionCell.appendChild(takeButton);
 
         const completeButton = document.createElement('button');
         completeButton.textContent = "Complete";
         completeButton.addEventListener('click', () => completeOrder(order.id));
         completeButton.id = `completeButton_${order.id}`;
-        orderItem.appendChild(completeButton);
-
-        orderList.appendChild(orderItem);
+        actionCell.appendChild(completeButton);
 
         updateButtonStates(order.id, order.status);
     }
 }
+
 
 function fetchOrders() {
     fetch('/rest/getOrders')
@@ -86,11 +92,12 @@ function updateOrderStatus(orderId, status) {
         completeButton.disabled = true;
     }
 
-    const orderItem = document.querySelector(`li[data-order-id="${orderId}"]`);
-    if (orderItem) {
-        const statusElement = orderItem.querySelector('.status');
+    // Находим строку (row) с соответствующим orderId
+    const row = document.querySelector(`tr[data-order-id="${orderId}"]`);
+    if (row) {
+        const statusElement = row.querySelector('.status');
         if (statusElement) {
-            statusElement.textContent = `(${status})`;
+            statusElement.textContent = status; // Обновляем текст статуса
         }
     }
 }
